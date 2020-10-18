43403b7db78b7d3490bc2b8c4d08ef7702855b91
Kynan Stewart <k7n4n5t3w4rt@users.noreply.github.com>
2020-10-18 14:06:32 +1100

    Create testrunner.js

---
+++ testrunner.js
@@ -0,0 +1,14 @@
+function pass(message) {
+  return `<p style="color: green">PASS: ${message}</p>`;
+}
+function fail(message) {
+  return `<p style="color: red">FAIL: ${message}</p>`;
+}
+function Test(message, testFunction) {
+  //console.log("test", testFunction());
+  const result = testFunction();
+  const li = document.createElement("li");
+  li.innerHTML = message + result;
+  document.getElementById("list").appendChild(li);
+  //console.log("message", message);
+}
